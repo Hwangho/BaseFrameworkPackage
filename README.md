e00f76a2023008a83beab0a196b12b0fbd2a950f
Hwangho <63707317+Hwangho@users.noreply.github.com>
2022-08-17 14:48:46 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BaseFrameworkPackage
+
+A description of this package.
---
+++ Sources/BaseFrameworkPackage/BaseFrameworkPackage.swift
@@ -0,0 +1,6 @@
+public struct BaseFrameworkPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
