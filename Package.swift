537159dd9abe331f2f0d7c5486d46e224e273eea
Hwangho <63707317+Hwangho@users.noreply.github.com>
2022-08-17 17:22:13 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BaseFrameworkPackage",
-            dependencies: []),
+            dependencies: ["BaseFramework"]),
+        .binaryTarget(
+          name: "BaseFramework",
+          path: "BaseFramework/BaseFramework.xcframework"),
         .testTarget(
             name: "BaseFrameworkPackageTests",
             dependencies: ["BaseFrameworkPackage"]),
